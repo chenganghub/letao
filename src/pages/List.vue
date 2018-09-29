b71d67722283f3bcef87f4363ca5de56a40fdec8
yang <yang_0430@qq.com>
2018-09-29 19:07:57 +0800

    分类页面头图跟随标签切换

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -96,14 +96,14 @@ export default {
   },
   methods: {
     liClick (val, id) {
-      this.isactive = val;
+      this.isactive = val
       // console.log(this.list[id].icon)
-      //点击时更改图标样式
+      // 点击时更改图标样式
       this.list[id].icon = !this.list[id].icon
-      //点击时修改排序类型参数
+      // 点击时修改排序类型参数
       this.sortlist.sort = id
       // console.log(this.sort)
-      //点击时修改排序方式参数
+      // 点击时修改排序方式参数
        this.sortlist.sorttype = this.list[id].icon==true?1:2
        console.log(this.sortlist)
        getList(this.sortlist)
@@ -175,12 +175,48 @@ export default {
       font-size: 16px;
       margin-top: 15px;
     }
+<<<<<<< HEAD
     p:nth-child(3) {
       font-size: 15px;
       color: red;
       margin-top: 15px;
       margin-bottom: 15px;
       > s {
+=======
+  }
+  .list{
+    display: flex;
+    width: 92%;
+    margin: 0 auto;
+    flex-wrap: wrap;
+    overflow: hidden;
+    justify-content: space-between;
+    box-sizing: border-box;
+    li{
+      width:48%;
+
+      img{
+        width:100%;
+        margin-top: 20px;
+      }
+      p:nth-child(2){
+        font-size: 16px;
+        margin-top: 15px;
+      }
+       p:nth-child(3){
+        font-size: 15px;
+        color:red;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        >s{
+          font-size: 12px;
+          margin-left: 10px;
+          color:#999;
+
+        }
+      }
+       p:nth-child(4){
+>>>>>>> 分类页面头图跟随标签切换
         font-size: 12px;
         margin-left: 10px;
         color: #999;
@@ -206,5 +242,9 @@ export default {
       color: white;
     }
   }
+<<<<<<< HEAD
 }
 </style>
+=======
+</style>
+>>>>>>> 分类页面头图跟随标签切换
